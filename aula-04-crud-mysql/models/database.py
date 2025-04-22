244811499c2d604067c9c47782433286e8a20c7f
Rafaelferrte <rafaelferrete333@gmail.com>
2025-04-22 11:56:18 -0300

    update

---
+++ aula-04-crud-mysql/models/database.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Carregando o SQLAlchemy na variavel DB
+db = SQLAlchemy()
+
+# Criando o Model (classe)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(150))
+    ano = db.Column(db.String(150))
+    categoria = db.Column(db.String(150))
+    plataforma = db.Column(db.String(150))
+    preco = db.Column(db.Float)
+    quantidade = db.Column(db.Integer)
+    
+# Método construtor da classe
+def __init__(self, titulo, ano, categoria, plataforma, preco, quantidade):
+    self.titulo = titulo
+    self.ano = ano
+    self.categoria = categoria
+    self.plataforma = plataforma
+    self.preco = preco
+    self.quantidade = quantidade
